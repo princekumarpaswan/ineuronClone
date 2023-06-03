75dc4c5c51f7eb2ce036f5bb71f43158154cce32
princekumarpaswan <prasantandal123@gmail.com>
2023-06-03 18:00:27 +0530

    completed clone

---
+++ script.js
@@ -0,0 +1,35 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 2500,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+let twoswiper = new Swiper(".twoSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 2500,
+    }
+});
+
+let threeswiper = new Swiper(".threeSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 2500,
+    }
+});
+
+
+var lgswiper = new Swiper(".lgmySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+    }
+});
